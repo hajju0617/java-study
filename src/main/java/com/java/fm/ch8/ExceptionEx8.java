c03ef573d9eac33e79a49196ced7ad46624f6888
hajju0617 <joowon023617@gmail.com>
2024-06-11 19:19:38 +0900

    ch8

--- src/main/java/com/java/fm/ch8/ExceptionEx8.java
+++ src/main/java/com/java/fm/ch8/ExceptionEx8.java
@@ -4,14 +4,18 @@ public class ExceptionEx8 {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
-        try {
+
+        try{
             System.out.println(3);
             System.out.println(0/0);
             System.out.println(4);
+            System.out.println(5);
         } catch (ArithmeticException ae) {
             ae.printStackTrace();
-            System.out.println("예외 메시지 : " + ae.getMessage());
+            System.out.println("예외 메세지 출력 : " + ae.getMessage());
         }
+
         System.out.println(6);
+        System.out.println(7);
     }
 }
