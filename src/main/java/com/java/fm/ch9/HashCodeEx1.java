2ba069a5c61d5563139de9ac025fd8a54993a4d3
hajju0617 <joowon023617@gmail.com>
2024-06-13 18:31:16 +0900

    ch9

---
+++ src/main/java/com/java/fm/ch9/HashCodeEx1.java
@@ -0,0 +1,23 @@
+package com.java.fm.ch9;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class HashCodeEx1 {
+    public static void main(String[] args) {
+
+        String str1 = new String("abc");
+        String str2 = new String("abc");
+
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.hashCode());
+        System.out.println(str2.hashCode());
+        System.out.println(System.identityHashCode(str1));
+        System.out.println(System.identityHashCode(str2));
+
+
+    }
+
+
+
+
+}
