9e3b754ce7929d22c7e7231738a60fd9ea36c87e
hajju0617 <joowon023617@gmail.com>
2024-06-17 23:59:51 +0900

    ch9

--- src/main/java/com/java/fm/ch9/EqualsEx1.java
+++ src/main/java/com/java/fm/ch9/EqualsEx1.java
@@ -3,25 +3,24 @@ package com.java.fm.ch9;
 public class EqualsEx1 {
     public static void main(String[] args) {
 
-        Value v1 = new Value(10);
-        Value v2 = new Value(10);
+
+        Value v1 = new Value(100);
+        Value v2 = new Value(100);
 
         if(v1.equals(v2)) {
             System.out.println("v1과 v2는 같다");
-        } else {
+        }   else {
             System.out.println("v1과 v2는 다름");
         }
 
         v2 = v1;
 
-
         if(v1.equals(v2)) {
             System.out.println("v1과 v2는 같다");
-        } else {
+        }   else {
             System.out.println("v1과 v2는 다름");
         }
     }
-
 }
 
 
