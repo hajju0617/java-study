327556c182c53de160c599067edb70980b3a2cb5
hajju0617 <joowon023617@gmail.com>
2024-06-19 19:47:24 +0900

    ch9

--- src/main/java/com/java/fm/ch9/CloneEx2.java
+++ src/main/java/com/java/fm/ch9/CloneEx2.java
@@ -1,7 +1,19 @@
 package com.java.fm.ch9;
-
+import java.util.*;
 public class CloneEx2 {
     public static void main(String[] args) {
-        System.out.println();
+
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] arrClone = arr.clone();
+        /*
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] arrClone = new int[arr.length];               // 배열 생성
+        System.arraycopy(arr, 0, arrClone, 0, arr.length);  // 내용을 복사
+         */
+
+        arrClone[0] = 6;
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arrClone));
     }
 }
