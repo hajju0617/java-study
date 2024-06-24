c8e066beeca36f1c80cb817ab50cfa98251590c2
hajju0617 <joowon023617@gmail.com>
2024-06-24 19:29:33 +0900

    ch9

--- src/main/java/com/java/fm/ch9/StringEx3.java
+++ src/main/java/com/java/fm/ch9/StringEx3.java
@@ -8,5 +8,9 @@ public class StringEx3 {
 
         System.out.println("cArr.length = " + cArr.length);
         System.out.println("@@@" + s + "@@@");
+
+        char[] Arr = {'H', 'e', 'l', 'l', 'o'};
+        String str = new String(Arr);
+        System.out.println(Arr);
     }
 }
