2aeeeb2d47eecaae413cd2964260d74fa3eea89d
hajju0617 <joowon023617@gmail.com>
2024-07-21 00:12:21 +0900

    연습

---
+++ src/main/java/com/java/fm/alrorithm/temp2.java
@@ -0,0 +1,13 @@
+package com.java.fm.alrorithm;
+
+public class temp2 {
+    private final int num;
+
+    public temp2(int num) {
+        this.num = num;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
