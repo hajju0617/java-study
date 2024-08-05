9578dfc173979593d604e18224f55266a2f064ea
hajju0617 <joowon023617@gmail.com>
2024-08-05 09:35:14 +0900

    test

--- src/main/java/com/java/fm/ch9/ToStringTest.java
+++ src/main/java/com/java/fm/ch9/ToStringTest.java
@@ -9,5 +9,12 @@ public class ToStringTest {
         System.out.println(str.toString());
         System.out.println(today);
         System.out.println(today.toString());
+
+        String s = "012345";
+        String s2 = s.replace("", "X");
+        System.out.println(s2);
+
+        String s3 = s.replace("", "X");
+        System.out.println(s3);
     }
 }
