f91e91129395b4310367637a827fd4f5f72a56c5
hajju0617 <joowon023617@gmail.com>
2024-11-05 02:19:42 +0900

    ch4 - 복습 중

--- src/main/java/com/java/fm/ch4/FlowEx21.java
+++ src/main/java/com/java/fm/ch4/FlowEx21.java
@@ -3,17 +3,17 @@ package com.java.fm.ch4;
 
 public class FlowEx21 {
     public static void main(String[] args) {
-        for(int i=1; i<=5; i++) {
-            for(int j=1; j<=5; j++) {
-                if (i==j) {
-                    System.out.printf("[%d,%d]", i,j);
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == j) {
+                    System.out.printf("[%d, %d]", i, j);
+                } else if (i + j == 6) {
+                    System.out.printf("[%d, %d]", i, j);
                 } else {
-                    System.out.printf("%5c", ' '); // 공백 채워주기
+                    System.out.printf("%5c", ' ');
                 }
-
             }
             System.out.println();
-
         }
     }
 }
--- src/main/java/com/java/fm/ch4/FlowEx22.java
+++ src/main/java/com/java/fm/ch4/FlowEx22.java
@@ -2,15 +2,17 @@ package com.java.fm.ch4;
 
 public class FlowEx22 {
     public static void main(String[] args) {
-        int[] arr = {10,20,30,40,50};
+        int[] arr = {10, 20, 30, 40, 50};
         int sum = 0;
 
-        for(int i=0; i<arr.length; i++) {
+        System.out.println("일반 for문");
+        for (int i = 0; i < arr.length; i++) {
             System.out.printf("%d ", arr[i]);
         }
         System.out.println();
 
-        for(int tmp : arr) {
+        System.out.println("향상된 for문");
+        for (int tmp : arr) {
             System.out.printf("%d ", tmp);
             sum += tmp;
         }
