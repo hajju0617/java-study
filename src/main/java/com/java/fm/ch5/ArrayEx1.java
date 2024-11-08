652ca1a418d9da85afd95eeac0a2724ab7c0767c
hajju0617 <joowon023617@gmail.com>
2024-11-09 03:30:00 +0900

    ch5 - 복습

--- src/main/java/com/java/fm/ch5/ArrayEx1.java
+++ src/main/java/com/java/fm/ch5/ArrayEx1.java
@@ -4,18 +4,28 @@ public class ArrayEx1 {
     public static void main(String[] args) {
         int[] score = new int[5];
         int k = 1;
+
         score[0] = 50;
         score[1] = 60;
         score[k+1] = 70;
         score[3] = 80;
         score[4] = 90;
+        int tmp = score[k + 2] + score[4];
 
-        int tmp = score[k+2] + score[4];
-
-        for(int i = 0; i < score.length; i++) {
-            System.out.printf("score[%d] = %d\n", i, score[i]);
+        System.out.println("일반 for문");
+        for (int i = 0; i < score.length; i++) {
+            System.out.printf("score[%d] = %d%n", i, score[i]);
         }
-        System.out.printf("tmp = %d\n", tmp);
-        //System.out.printf("score[%d] = %d\n", 7,score[7]); (배열의 인덱스의 범위를 넘어간 코드)
+
+        System.out.println("===========");
+
+        System.out.println("향상된 for문");
+        for (int i : score) {
+            System.out.println(i);
+        }
+        System.out.println("tmp = " + tmp);
+
+//        System.out.printf("score[%d] = %d%n", 5, score[5]);
+        // 배열의 인덱스 범위를 초과해버림. (ArrayIndexOutOfBoundsException 발생)
     }
 }
