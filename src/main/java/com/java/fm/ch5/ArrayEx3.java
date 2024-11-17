7f6d08b1feca8dc8ce39cde906182fb2ef10f7f4
hajju0617 <joowon023617@gmail.com>
2024-11-17 23:29:58 +0900

    ch5 - 복습

--- src/main/java/com/java/fm/ch5/ArrayEx3.java
+++ src/main/java/com/java/fm/ch5/ArrayEx3.java
@@ -1,29 +1,28 @@
 package com.java.fm.ch5;
 
+import java.util.Arrays;
+
 public class ArrayEx3 {
     public static void main(String[] args) {
         int[] arr = new int[5];
-
-        System.out.println("변경전");
-        System.out.println("arr.length : " + arr.length);
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 1;
         }
-        for(int i = 0; i < arr.length; i++) {
-            System.out.printf("arr[%d] : %d\n", i, arr[i]);
+        System.out.println("== 변경전 ==");
+        System.out.println("arr.length = " + arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("arr[" + i + "] = " + arr[i]);
         }
 
-        System.out.println("변경 후");
-        int[] arr2 = new int[arr.length * 2];
-        for(int i = 0; i < arr.length; i++) {
-            arr2[i] = arr[i];
+        int[] tmp = new int[arr.length * 2];        // 배열은 생성과 동시에 각 요소를 배열의 타입의 기본값으로 초기화.
+        for (int i = 0; i < arr.length; i++) {      // int[] 이므로 기본값 0으로 초기화 시켜줌.
+            tmp[i] = arr[i];
         }
-
-        arr = arr2;
-        System.out.println("arr.length : " + arr.length);
-        for(int i = 0; i < arr.length; i++) {
-            System.out.printf("arr[%d] = %d\n", i, arr[i]);
+        arr = tmp;
+        System.out.println("== 변경후 ==");
+        System.out.println("arr.length = " + arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("arr[" + i + "] = " + arr[i]);
         }
-
     }
 }
