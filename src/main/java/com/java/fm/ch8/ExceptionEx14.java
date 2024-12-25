cf966273e0ceeb31a8a0a8d79c7b03d7f3a56bec
hajju0617 <joowon023617@gmail.com>
2024-12-25 23:35:17 +0900

    ch8 - 메서드 선언부에 예외 선언 처리 방식 복습.

--- src/main/java/com/java/fm/ch8/ExceptionEx14.java
+++ src/main/java/com/java/fm/ch8/ExceptionEx14.java
@@ -1,18 +1,14 @@
 package com.java.fm.ch8;
 
 public class ExceptionEx14 {
-
     public static void main(String[] args) {
-
         try {
             method1();
         } catch (Exception e) {
-            System.out.println("main 메서드에서 예외가 처리 되었음");
+            System.out.println("method1의 예외가 main메서드에서 처리 되었음.");
             e.printStackTrace();
         }
-
     }
-
     static void method1() throws Exception {
         throw new Exception();
     }
--- src/main/java/com/java/fm/ch8/ExceptionEx15.java
+++ src/main/java/com/java/fm/ch8/ExceptionEx15.java
@@ -4,26 +4,25 @@ import java.io.*;
 public class ExceptionEx15 {
     public static void main(String[] args) {
         File f = createFile(args[0]);
-        System.out.println( f.getName() + " 파일이 성공적으로 생성");
+        System.out.println(f.getName() + " 파일이 성공적으로 생성되었음.");
     }
 
-    static File createFile (String fileName) {
+    static File createFile(String fileName) {
         try {
             if (fileName == null || fileName.equals("")) {
-                throw new Exception("파일 이름이 유효하지 않음");
+                throw new Exception("파일이름이 유요하지 않음.");
             }
         } catch (Exception e) {
-            fileName = "부적절.txt";
+            fileName = "제목없음.txt";          // 파일이름이 이상할 경우 "제목없음.txt"로 설정.
         } finally {
             File f = new File(fileName);
-            createNewFile(f);
+            createNewFile(f);               // 만든 객체를 이용해서 파일 생성.
             return f;
         }
     }
-
-    static void createNewFile (File f) {
+    static void createNewFile(File f) {
         try {
-            f.createNewFile();
+            f.createNewFile();      // 파일 생성.
         } catch (Exception e) {
 
         }
